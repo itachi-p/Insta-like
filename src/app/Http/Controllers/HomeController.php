24cd5d5f68ac1475619773e2754d0848c65dab39
itachi-P <itachip38@gmail.com>
2019-10-08 11:36:05 +0900

    Cleared Imagefiles upload & shows.

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -6,10 +6,14 @@ use App\Model\Image;
 
 class HomeController extends Controller
 {
-    public function home()
+    public function home(Request $request)
     {
         $images = Image::all();
-        return view('home', ['images' => $images]);
+//dd($images[0]); //debug
+        // (仮)
+        $request->merge(['user_id' => $images[0]['user_id'],]);
+//dd($request); //debug
+        return view('home', ['images' => $images, 'request' => $request]);
     }
 
     public function upload(Request $request)
@@ -28,14 +32,20 @@ class HomeController extends Controller
             //$path = $request->file->store('public');
             $filename = $request->file->getClientOriginalName(); //一意なID発行の方が望ましい
             $move = $request->file->move('./images/', $filename);
+            $message = $request->message;
 
             //仮実装
-            //$user_id = '1';
+//dd($request); //debug
+            $user_id = $request->user_id;
+echo "user_id" . $user_id . "<br>";
 
             $images = new Image;
             $images->fill(['user_id' => $user_id, 'filename' => $filename])->save();
+echo "images->fill->save() finished. <br>";
             $images = Image::all();
             $parameters = ['user_id' => $user_id, 'filename' => $filename, 'images' => $images];
+echo "\$parameters setting finished.";
+//dd($parameters); //debug
             return view('home', $parameters);
         } else {
             return redirect()
