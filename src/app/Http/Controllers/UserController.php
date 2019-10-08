0ce7f0fd4e952b9d14604ec9fe7bbca984bdb925
itachi-P <itachip38@gmail.com>
2019-10-08 09:21:43 +0900

    Modified user data getting from User model.

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -11,11 +11,13 @@ class UserController extends Controller
         $users = User::all()->toArray();
 //var_dump($users); //debug
 //dd($users); //debug
-        $username = $users[0]['name'];
+        $user_id = $users[0]['id'];
+        $user_name = $users[0]['name'];
 
         $request = new Request;
         $request->merge([
-            'username' => $username,
+            'user_id' => $user_id,
+            'user_name' => $user_name,
         ]);
         return view('user', $request);
     }
