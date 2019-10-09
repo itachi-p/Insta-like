859c8de178b6a54ef5811703b9ef6036cfce0a35
itachi-P <itachip38@gmail.com>
2019-10-09 23:45:48 +0900

    Rename application root directory from src to laravel-app.

---
+++ laravel-app/app/Http/Controllers/HomeController.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Image;
+
+class HomeController extends Controller
+{
+    public function home(Request $request)
+    {
+        $images = Image::all();
+//dd($images[0]); //debug
+        // (仮)
+        foreach ($images as $image) {
+            $request->merge(['user_id' => $image->user_id,]);
+        }
+//dd($request); //debug
+        return view('home', ['images' => $images, 'request' => $request]);
+    }
+
+    public function upload(Request $request)
+    {
+//dd($request);       
+        $this->validate($request, [
+            'file' => [
+                'required', // 入力必須であること    
+                'file',     // アップロードされたファイルであること
+                'image',    // 画像ファイルであること
+                'mimes:jpeg,png,gif',   // MIMEタイプを指定
+            ]
+        ]);
+
+        if ($request->file('file')->isValid([])) {
+            //storeメソッドを使うとlocalhost:8000に固定されてしまうので他のやり方に回避(画像ファイル名加工なし)
+            //$path = $request->file->store('public');
+            $filename = $request->file->getClientOriginalName(); //一意なID発行の方が望ましい
+            $move = $request->file->move('./images/', $filename);
+            $message = $request->message;
+
+            //仮実装
+//dd($request); //debug
+            $user_id = $request->user_id;
+echo "user_id" . $user_id . "<br>";
+
+            $images = new Image;
+            $images->fill(['user_id' => $user_id, 'filename' => $filename])->save();
+echo "images->fill->save() finished. <br>";
+            $images = Image::all();
+            $parameters = ['user_id' => $user_id, 'filename' => $filename, 'images' => $images];
+echo "\$parameters setting finished.";
+//dd($parameters); //debug
+            return view('home', $parameters);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors();
+        }
+    }
+}
---
+++ laravel-app/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+# Laravelがデフォルトで用意してるmigrationを利用したusersテーブルを利用
+Route::get('/user', 'UserController@index');
+
+Route::get('/home', 'HomeController@home');
+Route::post('/upload', 'HomeController@upload');
